a4a282c9f91be514e8803cd71b3617660549eb0e
Kiu L. <zikethemike@yahoo.com>
2024-02-21 22:30:58 -0800

    add 2 node modules
    react-router-dom
    vite-tsconfig-paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import { HomePage } from '@Pages/home-page'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <HomePage />,
+	},
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 )
